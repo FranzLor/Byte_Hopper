fb79cc962a958a550aaf12a16d0fe445b7fd8435
Franz <FranzLor64@yahoo.com>
2024-08-31 15:59:03 -0600

    Player: Animator States
    updated animator states with animator controller foundation
    updated animators

--- Assets/Byte Hopper/Scripts/AnimatorController.cs
+++ Assets/Byte Hopper/Scripts/AnimatorController.cs
@@ -4,5 +4,33 @@ using UnityEngine;
 
 public class AnimatorController : MonoBehaviour
 {
+    public PlayerController playerController = null;
+    private Animator animator = null;
 
+    void Start()
+    {
+        animator = this.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (playerController.isDead)
+        {
+            animator.SetBool("Dead", true);
+        }
+
+        if (playerController.jumpStart)
+        {
+            animator.SetBool("Fly Forward 02 In Place", true);
+        }
+        else if (playerController.isJumping)
+        {
+            animator.SetBool("Fly Forward", true);
+        }
+        else
+        {
+            animator.SetBool("Fly Forward 02 In Place", false);
+            animator.SetBool("Fly Forward", false);
+        }
+    }
 }
