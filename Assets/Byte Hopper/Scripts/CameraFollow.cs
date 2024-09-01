186057a80b985651e9a42a16298d490a0c92843b
Franz <FranzLor64@yahoo.com>
2024-09-01 15:40:04 -0600

    Camera: Follow Setup
    updated camera follow script with main variables and update

--- Assets/Byte Hopper/Scripts/CameraFollow.cs
+++ Assets/Byte Hopper/Scripts/CameraFollow.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
+    public bool autoMove = true;
 
+    public GameObject player = null;
 
+    public Vector3 offset = new Vector3(0, 0, 0);
+    Vector3 depth = Vector3.zero;
+    Vector3 position = Vector3.zero;
+
+    void Update()
+    {
+
+    }
 }
