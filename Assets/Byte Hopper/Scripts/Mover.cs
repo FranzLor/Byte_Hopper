6c57cdcdcc1a7f2232b117d7f5b0a111542ca706
Franz <FranzLor64@yahoo.com>
2024-09-01 00:35:55 -0600

    Player: Mover Method Setups
    updated mover script with main method foundations

--- Assets/Byte Hopper/Scripts/Mover.cs
+++ Assets/Byte Hopper/Scripts/Mover.cs
@@ -15,5 +15,38 @@ public class Mover : MonoBehaviour
     private Renderer renderer = null;
     private bool isVisible = false;
 
+    void Start()
+    {
+        renderer = moverObject.GetComponent<Renderer>();
+    }
 
+    void Update()
+    {
+        this.transform.Translate(0, 0, speed * Time.deltaTime);
+        IsVisible();
+    }
+
+    void IsVisible()
+    {
+        if (renderer.isVisible)
+        {
+            isVisible = true;
+        }
+        if (!renderer.isVisible && isVisible)
+        {
+            Debug.Log("Object is no longer visible");
+
+            Destroy(this.gameObject);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+
+    }
 }
