09a1a82827559c413bdd0591af0859fa44f43b9d
Franz <FranzLor64@yahoo.com>
2024-09-02 21:18:40 -0600

    Spawner: Setup
    updated spawner script with setup variables and methods foundation

--- Assets/Byte Hopper/Scripts/Spawner.cs
+++ Assets/Byte Hopper/Scripts/Spawner.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform startPosition = null;
+
+    public float delayMin = 2.0f;
+    public float delayMax = 5.0f;
+
+    public float speedMin = 1.0f;
+    public float speedMax = 6.0f;
+
+    public bool useSpawnPlacement = false;
+    public int spawnCountMin = 4;
+    public int spawnCountMax = 18;
+
+    private float lastTime = 0.0f;
+    private float delayTime = 0.0f;
+
+    private float speed = 0.0f;
+
+    // spawned objects
+    [HideInInspector] public GameObject item = null;
+
+    [HideInInspector] public bool goLeft = false;
+
+    [HideInInspector] public float spawnLeftPosition = 0.0f;
+
+    [HideInInspector] public float spawnRightPosition = 0.0f;
+
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    void SpawnItem()
+    {
+
+    }
+
+    Vector3 GetSpawnPosition()
+    {
+        // TODO: error check fn
+        return Vector3.zero;
+    }
+
 }
