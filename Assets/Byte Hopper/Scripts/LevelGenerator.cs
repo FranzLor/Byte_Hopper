f247d2790bcb86d9ee0604fe089c95082dfe05b4
Franz <FranzLor64@yahoo.com>
2024-09-04 13:53:44 -0600

    LevelGen: Random Generator
    updated level generator script with main random gen method logic and instantiation

--- Assets/Byte Hopper/Scripts/LevelGenerator.cs
+++ Assets/Byte Hopper/Scripts/LevelGenerator.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
 {
-    public List<GameOject> platform = new List<GameObject>();
+    public List<GameObject> platform = new List<GameObject>();
     // track platform height to account for different platform heights
     public List<float> height = new List<float>();
 
@@ -14,12 +14,32 @@ public class LevelGenerator : MonoBehaviour
 
     public void randomGenerator()
     {
+        randomRange = Random.Range(0, platform.Count);
 
+        for (int i = 0; i < platform.Count; i++)
+        {
+            CreateLevelObject(platform[i], height[i], i);
+        }
     }
 
     public void CreateLevelObject(GameObject obj, float height, int value)
     {
+        if (randomRange == value)
+        {
+            GameObject gameObj = Instantiate(obj) as GameObject;
 
+            // get half value to center the object
+            float offset = lastPosition + (lastScale * 0.5f);
+
+            offset += (gameObj.transform.localScale.z) * 0.5f;
+            Vector3 position = new Vector3(0, height, offset);
+
+            gameObj.transform.position = position;
+            lastPosition = gameObj.transform.position.z;
+            lastScale = gameObj.transform.localScale.z;
+
+            gameObj.transform.parent = this.transform;
+        }
     }
 }
  
