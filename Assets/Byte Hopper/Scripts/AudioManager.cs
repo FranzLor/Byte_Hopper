fcb68a34d8cbf85964de06dfbf70b9ec9b307ff7
Franz <FranzLor64@yahoo.com>
2024-09-13 16:57:48 -0600

    Manager: Main Functions
    updated audio manager script main functionality
    -we call this now

--- Assets/Byte Hopper/Scripts/AudioManager.cs
+++ Assets/Byte Hopper/Scripts/AudioManager.cs
@@ -4,58 +4,61 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-
     public Sound[] musicSounds, sfxSounds;
     public AudioSource musicSource, sfxSource;
 
+    public static AudioManager instance;
 
-    //// main audio sources
-    //public AudioSource musicSource = null;
-    //public AudioSource sfxSource = null;
+    private void Awake()
+    {
+        // singleton
+        // create only one instance of audio manager
+        if (instance == null)
+        {
+            instance = this;
+            // carry audio manager through scenes
+            DontDestroyOnLoad(gameObject);
+        } 
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 
-    //// background sfx
-    //public AudioClip ambience = null;
+    public void Start()
+    {
+        PlayMusic("Theme");
+    }
 
-    //// UI SFX
-    //public AudioClip buttonSFX = null;
+    public void PlayMusic(string name)
+    {
+        Sound s = System.Array.Find(musicSounds, sound => sound.name == name);
 
-    //// player sfx
-    //public AudioClip idleSFX1 = null;
-    //public AudioClip idleSFX2 = null;
-    //public AudioClip hitSFX = null;
-    //public AudioClip splashSFX = null;
-    //public AudioClip electricSFX = null;
-    //public AudioClip coinSFX = null;
-    //public AudioClip jumpSFX = null;
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found!");
+            return;
+        }
+        else
+        {
+            musicSource.clip = s.clip;
+            musicSource.Play();
+        }
+    }
 
-    //public static AudioManager instance;
+    public void PlaySFX(string name)
+    {
+        Sound s = System.Array.Find(sfxSounds, sound => sound.name == name);
 
-    //private void Awake()
-    //{
-    //    // singleton
-    //    // creates only one instance of the audio manager
-    //    if (instance == null)
-    //    {
-    //        instance = this;
-    //        // carries audio manager through scenes
-    //        DontDestroyOnLoad(gameObject);
-    //    }
-    //    else
-    //    {
-    //        Destroy(gameObject);
-    //    }
-        
-    //}
-
-    //public void Start()
-    //{
-    //    musicSource.clip = ambience;
-    //    musicSource.Play();
-    //}
-
-
-    //public void PlaySFX(AudioClip clip)
-    //{
-    //    sfxSource.PlayOneShot(clip);
-    //}
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found!");
+            return;
+        }
+        else
+        {
+            sfxSource.clip = s.clip;
+            sfxSource.Play();
+        }
+    }
 }
