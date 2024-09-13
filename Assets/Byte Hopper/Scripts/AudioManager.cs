0323d282e6ab87ba2361a4e43657f8dcc16d472f
Franz <FranzLor64@yahoo.com>
2024-09-13 16:50:54 -0600

    Manager: Sound  & Manager Scripts
    updated manager script
    added manager array for sfx and music

--- Assets/Byte Hopper/Scripts/AudioManager.cs
+++ Assets/Byte Hopper/Scripts/AudioManager.cs
@@ -4,15 +4,58 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
+    public Sound[] musicSounds, sfxSounds;
+    public AudioSource musicSource, sfxSource;
+
+
+    //// main audio sources
+    //public AudioSource musicSource = null;
+    //public AudioSource sfxSource = null;
+
+    //// background sfx
+    //public AudioClip ambience = null;
+
+    //// UI SFX
+    //public AudioClip buttonSFX = null;
+
+    //// player sfx
+    //public AudioClip idleSFX1 = null;
+    //public AudioClip idleSFX2 = null;
+    //public AudioClip hitSFX = null;
+    //public AudioClip splashSFX = null;
+    //public AudioClip electricSFX = null;
+    //public AudioClip coinSFX = null;
+    //public AudioClip jumpSFX = null;
+
+    //public static AudioManager instance;
+
+    //private void Awake()
+    //{
+    //    // singleton
+    //    // creates only one instance of the audio manager
+    //    if (instance == null)
+    //    {
+    //        instance = this;
+    //        // carries audio manager through scenes
+    //        DontDestroyOnLoad(gameObject);
+    //    }
+    //    else
+    //    {
+    //        Destroy(gameObject);
+    //    }
         
-    }
+    //}
+
+    //public void Start()
+    //{
+    //    musicSource.clip = ambience;
+    //    musicSource.Play();
+    //}
+
+
+    //public void PlaySFX(AudioClip clip)
+    //{
+    //    sfxSource.PlayOneShot(clip);
+    //}
 }
