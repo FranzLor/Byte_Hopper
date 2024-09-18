81a75f85784efba72e9866b9786732537e90cdb9
Franz <FranzLor64@yahoo.com>
2024-09-18 11:10:39 -0600

    UI: Stat Tracker UI Interaction
    updated stat tracker script to close and open screen
    updated UI canvas button interactions
    -start screen to stat tracker screen
    -stat tracker to start screen

--- Assets/Byte Hopper/Scripts/StatTrackerManager.cs
+++ Assets/Byte Hopper/Scripts/StatTrackerManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class StatTrackerManager : MonoBehaviour
 {
@@ -16,6 +17,10 @@ public class StatTrackerManager : MonoBehaviour
     public TMP_Text deathByWaterText;
     public TMP_Text deathByMicrochipText;
     public TMP_Text deathByCameraText;
+
+    public GameObject statTrackerScreen;
+    public Button closeButton;
+    public GameObject startScreen;
 
     private void Awake()
     {
@@ -36,6 +41,70 @@ public class StatTrackerManager : MonoBehaviour
         UpdateUI();
     }
 
+    public void AddDistance(int distance)
+    {
+        int lifetimeDistance = PlayerPrefs.GetInt("LifetimeDistance", 0);
+
+        lifetimeDistance += distance;
+        PlayerPrefs.SetInt("LifetimeDistance", lifetimeDistance);
+
+        CheckForHighestDistance(distance);
+        UpdateUI();
+    }
+
+    public void AddCoins(int coins)
+    {
+        int lifetimeCoins = PlayerPrefs.GetInt("LifetimeCoins", 0);
+
+        lifetimeCoins += coins;
+        PlayerPrefs.SetInt("LifetimeCoins", lifetimeCoins);
+
+        UpdateUI();
+    }
+
+    public void AddDeath(string cause)
+    {
+        int lifetimeDeaths = PlayerPrefs.GetInt("LifetimeDeaths", 0);
+        lifetimeDeaths++;
+        PlayerPrefs.SetInt("LifetimeDeaths", lifetimeDeaths);
+
+        switch (cause)
+        {
+            case "Car":
+                int deathByCars = PlayerPrefs.GetInt("DeathByCars", 0);
+                deathByCars++;
+                PlayerPrefs.SetInt("DeathByCars", deathByCars);
+                break;
+            case "Water":
+                int deathByWater = PlayerPrefs.GetInt("DeathByWater", 0);
+                deathByWater++;
+                PlayerPrefs.SetInt("DeathByWater", deathByWater);
+                break;
+            case "Microchip":
+                int deathByMicrochip = PlayerPrefs.GetInt("DeathByMicrochip", 0);
+                deathByMicrochip++;
+                PlayerPrefs.SetInt("DeathByMicrochip", deathByMicrochip);
+                break;
+            case "Camera":
+                int deathByCamera = PlayerPrefs.GetInt("DeathByCamera", 0);
+                deathByCamera++;
+                PlayerPrefs.SetInt("DeathByCamera", deathByCamera);
+                break;
+        }
+
+        PlayerPrefs.Save();
+        UpdateUI();
+    }
+
+    private void CheckForHighestDistance(int newDistance)
+    {
+        int highestDistance = PlayerPrefs.GetInt("HighestDistance", 0);
+        if (newDistance > highestDistance)
+        {
+            PlayerPrefs.SetInt("HighestDistance", newDistance);
+        }
+    }
+
     private void LoadStats()
     {
         // load stats from player prefs
@@ -52,5 +121,17 @@ public class StatTrackerManager : MonoBehaviour
         deathByWaterText.text = PlayerPrefs.GetInt("DeathByWater", 0).ToString();
         deathByMicrochipText.text = PlayerPrefs.GetInt("DeathByMicrochip", 0).ToString();
         deathByCameraText.text = PlayerPrefs.GetInt("DeathByCamera", 0).ToString();
+    }
+
+    public void OpenStatTracker()
+    {
+        startScreen.SetActive(false);
+        statTrackerScreen.SetActive(true);
+    }
+
+    public void CloseStatTracker()
+    {
+        statTrackerScreen.SetActive(false);
+        startScreen.SetActive(true);
     }
 }
