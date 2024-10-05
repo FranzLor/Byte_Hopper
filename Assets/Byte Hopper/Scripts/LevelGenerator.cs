249e738cc7287ad91f14a3522ef4901f4f54ac6b
Franz <FranzLor64@yahoo.com>
2024-10-05 17:04:53 -0600

    Culling: Level Generator Culling
    updated level gen script and added culling system
    -adds random gen prefabs in list
    -list removes prefabs in culling distance (set to 20 default)

--- Assets/Byte Hopper/Scripts/LevelGenerator.cs
+++ Assets/Byte Hopper/Scripts/LevelGenerator.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:47cb79e88c6fa3d81ddcaf71680489ed444ccd6a049dd9fbad4c20ac495f1fd1
-size 1360
+oid sha256:d7e2a7c7f3f7685dda5b7909496437d8fd6dbcf6a8f1de44e52c2028bc747795
+size 2449
