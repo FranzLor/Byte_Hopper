2f7168d482c9da1afc2e16cf221cb79b46553ddf
Franz <FranzLor64@yahoo.com>
2024-10-07 02:06:00 -0600

    Prefab: Coin Spawn Overlap Refinement
    updated spawner script to add spawn position thresholds,
    -mainly for coin overlapping and adding more distance

--- Assets/Byte Hopper/Scripts/Spawner.cs
+++ Assets/Byte Hopper/Scripts/Spawner.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c3bcb07cebfb19fede0867f4c93686a4afe1820d128ecf1fd9b3064cb43a3127
-size 3345
+oid sha256:6c3001261b7032b0daecfa38f5d478148771e245af24158ea2159c4aad259d32
+size 3828
