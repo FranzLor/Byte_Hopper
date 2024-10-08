8e9d2e18f811a7aae94f225cf8c9f9f2a45d94cf
Franz <FranzLor64@yahoo.com>
2024-10-08 15:05:42 -0600

    Lighting: Present & Manager
    added main lighting preset script
    added lighting manager script

---
+++ Assets/Byte Hopper/Scripts/LightingManager.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:14d4ba81b3fe9a1172bc2bb399c51acc56d468519a2d8e2ec706bf3db1dede75
+size 317
---
+++ Assets/Byte Hopper/Scripts/LightingPreset.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3edcaa397eabe92911860f8a48cff6362f49f587df468d0452603546552baef7
+size 316
