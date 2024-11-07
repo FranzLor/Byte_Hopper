0443a9a1163ef999389d8949a96618768f762077
Franz Lor <FranzLor64@yahoo.com>
2024-11-07 16:36:36 -0700

    Player: Raycast Height Update
    updated player controller script and increased raycasting height
    -more accurate movement checks

--- Assets/Byte Hopper/Scripts/PlayerController.cs
+++ Assets/Byte Hopper/Scripts/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2f55f176a6efe45705a89f7520525f0fd3eb6ad0176155ec00e179245ff9ecca
-size 11186
+oid sha256:63aaeb19afd7dfb256b75639043f468bf4fbca86a66e7279d7e3c50a334bbc92
+size 11527
