675e52c5a136054abe6b33e519ad0f12ed21be69
Franz Lor <FranzLor64@yahoo.com>
2024-11-12 20:04:30 -0700

    Prefab: Train Light Fix
    updated and fixed train light indicators not lighting up
    -make sure trains have right tag

--- Assets/Byte Hopper/Scripts/LevelGenerator.cs
+++ Assets/Byte Hopper/Scripts/LevelGenerator.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:643116677de31780bc84e377e7efff7ea40062523e1d86e1764b5ca7459eebe2
-size 3441
+oid sha256:13aa11f9ebc104a4072d9396b0b1e42273668b793555adcf9cf213d10c477f90
+size 3773
