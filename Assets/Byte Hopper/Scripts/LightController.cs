ec365da69a64210b83517348542e23c17410a168
Franz Lor <FranzLor64@yahoo.com>
2024-11-14 18:01:54 -0700

    Skin-Shop: Lights Update
    updated lighting controller for ignoring lights with ignore tag

--- Assets/Byte Hopper/Scripts/LightController.cs
+++ Assets/Byte Hopper/Scripts/LightController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:69cc2c403df90793e988fb681e48531a91c7882ab90c04f0438cd2c715f50e6f
-size 1320
+oid sha256:dfa76a2d6307adf8512970bd6ab4a64d4a22f70181cf87fa8aadb55b0167d01b
+size 1689
--- Assets/Byte Hopper/Scripts/SpawnController.cs
+++ Assets/Byte Hopper/Scripts/SpawnController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f52eb9f3ec72abe7bb36d56b0ff8f18dbd84f30179e601e3eb8ac6286827856b
-size 3580
+oid sha256:1e059fff7ac55359d5354d0212eff19893169aa56d54af77babd779c06e952f0
+size 3753
