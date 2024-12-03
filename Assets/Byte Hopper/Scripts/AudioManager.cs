451d50e4da27ff05003e808fb7cc022a9c7c248a
Franz Lor <FranzLor64@yahoo.com>
2024-12-03 15:18:50 -0700

    SFX: Reworks
    updated SFX for eating, with crunch
    added game start SFX
    added game over SFX

--- Assets/Byte Hopper/Scripts/AudioManager.cs
+++ Assets/Byte Hopper/Scripts/AudioManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1b937018cc54b87b5ddcd51b83a78b7bc854072abcf114be301d1ef4e275d001
-size 3634
+oid sha256:93958ec9ae90b3a7ec5b55410d68d64f3c2f3664c68d1d16155b0a1f5dde2dd3
+size 3558
--- Assets/Byte Hopper/Scripts/Manager.cs
+++ Assets/Byte Hopper/Scripts/Manager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8af035222a3237b700750c7430a8a82d65abae8906fff545ff2d5100bcb31207
-size 4770
+oid sha256:0d8128145d0f521fddea57a6eeaf800d3ff73be76be3537b0d446ee5f2d409e7
+size 4947
