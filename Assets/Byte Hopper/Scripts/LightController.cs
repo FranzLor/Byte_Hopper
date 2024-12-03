20986b0758a73648fd74be11ad6fd23af0dc292f
Franz Lor <FranzLor64@yahoo.com>
2024-12-02 19:47:10 -0700

    Prefabs: Fixed Collider Positioning
    updated prefabs' collider positioning for better player to obj collision
    -ground1
    -highway2
    -railroad
    -spill 1 & 2

--- Assets/Byte Hopper/Scripts/LightController.cs
+++ Assets/Byte Hopper/Scripts/LightController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dfa76a2d6307adf8512970bd6ab4a64d4a22f70181cf87fa8aadb55b0167d01b
-size 1689
+oid sha256:f769c21c4d60dd575ca25087497d351245ac90204604e59512d9c5c1bb0c5c0b
+size 3021
--- Assets/Byte Hopper/Scripts/PlayerController.cs
+++ Assets/Byte Hopper/Scripts/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ccb1da35a61e026ec9e2f622285eaeff1deee769d6d9298693ab5c36f8db566b
-size 12253
+oid sha256:9e05667ea8e4735667083663191a6446fa3096e68ad0a22171ec65631df81db0
+size 11433
