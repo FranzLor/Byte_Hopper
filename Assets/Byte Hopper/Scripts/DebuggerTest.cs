f1090cfd7455c97570414da4be6a72d23b91baef
Franz Lor <FranzLor64@yahoo.com>
2024-12-02 20:28:44 -0700

    Debug: Debug Menu Update
    updated debugger menu to hide changeable attributes

--- Assets/Byte Hopper/Scripts/DebuggerTest.cs
+++ Assets/Byte Hopper/Scripts/DebuggerTest.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5c8bdc3c94e30f82c2060d626fc871c7d9f48c5689d80b2b86a1a6ee44af5bff
-size 2905
+oid sha256:775fe57fcecb816a74a8b7757cf4baecd2c987b0ae1bf6df8c896940d25b3cff
+size 2913
