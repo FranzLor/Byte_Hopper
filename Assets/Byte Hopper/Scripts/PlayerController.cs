dd047d6b5083b5580ee461fc794595ced26eddf5
Franz Lor <FranzLor64@yahoo.com>
2024-12-04 17:52:33 -0700

    Score: Revised & Fixed
    updated scoring for distance that fixes counting on side with forward movement

--- Assets/Byte Hopper/Scripts/PlayerController.cs
+++ Assets/Byte Hopper/Scripts/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7c39191d698bf09a35343273f3fe3a1f8f43e9fb5df629931a3093c2c4ecddb6
-size 10896
+oid sha256:eb1e9d2b4bb982016832299d5157924b427ade4f2b2774e82d5fc0ff2372a35a
+size 11006
